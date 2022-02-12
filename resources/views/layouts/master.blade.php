79f86cf02ed218fa2825234e4721a2672a982b3f
Sazid Hassan Khan <sazid.khan.hassan@gmail.com>
2022-02-12 22:27:56 +0600

    Footer And Base DB updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -60,18 +60,7 @@
             <a href="#nav-top" class="text-muted" title="Go to top"><i class="fas fa-angle-double-up"></i></a>
         </div>
         <div class="footer-text">
-            Copyright &copy; tahmid-ni7 - <span id="year"></span>
-        </div>
-        <div class="social-icon mt-2">
-        <span class="mr-2">
-          <a href="https://www.facebook.com/iamTahmid.ni7" class="text-primary"><i class="fab fa-facebook fa-2x"></i></a>
-        </span>
-            <span class="mr-2">
-          <a href="https://github.com/tahmid-ni7" class="text-secondary"><i class="fab fa-github fa-2x"></i></a>
-        </span>
-            <span class="mr-2">
-          <a href="https://stackoverflow.com/users/11026749/tahmid-ni7" class="text-warning"><i class="fab fa-stack-overflow fa-2x"></i></a>
-        </span>
+            Copyright &copy; -Sazid Hassan Khan & Nasif Muqarrabin - <span id="year"></span>
         </div>
     </div>
 </footer>
